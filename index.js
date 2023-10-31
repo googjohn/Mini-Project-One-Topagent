69b929eb134e408d11664e5b882ab29e79fb9e96
googjohn <120353235+googjohn@users.noreply.github.com>
2023-10-31 21:21:14 +0800

    improved navigation bar, added search and user tab, added sub section and other minor changes in background like parallax effect

--- index.js
+++ index.js
@@ -6,43 +6,43 @@ const textArray = [
 ]
 const delay = 1000;
 
-      let index = 0;
-      let currentText = "";
-      let isDeleting = false;
+let index = 0;
+let currentText = "";
+let isDeleting = false;
 
-      function type() {
-        const text = textArray[index];
-        if (isDeleting) {
-          currentText = text.substring(0, currentText.length - 1);
-        } else {
-          currentText = text.substring(0, currentText.length + 1);
-        }
+function type() {
+  const text = textArray[index];
+  if (isDeleting) {
+    currentText = text.substring(0, currentText.length - 1);
+  } else {
+    currentText = text.substring(0, currentText.length + 1);
+  }
 
-        typingText.innerHTML = currentText;
+  typingText.innerHTML = currentText;
 
-        let typeSpeed = 120;
+  let typeSpeed = 120;
 
-        if (isDeleting) {
-          typeSpeed /= 2;
-        }
+  if (isDeleting) {
+    typeSpeed /= 2;
+  }
 
-        if (!isDeleting && currentText === text) {
-          typeSpeed = delay;
-          isDeleting = true;
-        } else if (isDeleting && currentText === "") {
-          isDeleting = false;
-          index++;
-          if (index === textArray.length) {
-            index = 0;
-          }
-        }
+  if (!isDeleting && currentText === text) {
+    typeSpeed = delay;
+    isDeleting = true;
+  } else if (isDeleting && currentText === "") {
+    isDeleting = false;
+    index++;
+    if (index === textArray.length) {
+    index = 0;
+    }
+  }
 
-        setTimeout(() => {
-          type();
-        }, typeSpeed);
-      }
+  setTimeout(() => {
+    type();
+  }, typeSpeed);
+}
 
-      type();
+type();
 
 window.addEventListener('wheel', scrollToNextSection);
 
@@ -54,7 +54,8 @@ function scrollToNextSection(event) {
   if (delta < 0) {
     // Scrolling up
     const previousSection = document.querySelector('.active')?.previousElementSibling;
-
+    const activeNav = document.querySelector('.active')?.previousElementSibling;
+    const activeStyle = document.querySelector('.navlink a')
     if (previousSection) {
       previousSection.scrollIntoView({ behavior: 'smooth' });
       previousSection.classList.add('active');
@@ -83,7 +84,7 @@ function scrollToNextSection(event) {
   }
 }
 
-let btn = document.getElementById('back-to-top')
+let btn = document.getElementById('back-to-top');
 
 window.addEventListener('scroll', scrollToTop);
 function scrollToTop () {
